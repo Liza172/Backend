efec435e46fd19cf42daf8474c141a98a1030a9c
Liza172 <lizarakshit@gmail.com>
2025-02-15 21:55:31 +0530

    3rd ejs Dynamic Routing Complete ho gya

--- Day5_Master_EJS_Dynamic_Roiting_Project_setup/index.js
+++ Day5_Master_EJS_Dynamic_Roiting_Project_setup/index.js
@@ -12,6 +12,22 @@ app.get("/", function(req, res)
 {
   res.render("index");
 })
+//For dynamic routing
+
+
+app.get("/profile/:username", function(req,res){
+  //to acess th given username: - req.params.username
+  res.send(`welcome, ${req.params.username}`);
+})
+
+app.get("/profile/:username/:age", function(req,res){
+
+  res.send(`welcome, ${req.params.username} of ${req.params.age}`);
+})
+
+
+
+
 app.listen(3000, function()
 {
   console.log("its running");
